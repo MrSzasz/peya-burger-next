321b4c6b187cc165641a366135442c531edaf1f2
MrSzasz <lugoleandro@hotmail.com>
2023-07-30 18:06:22 -0300

    all countries done

---
+++ src/app/do/burgertour/page.jsx
@@ -0,0 +1,112 @@
+import MainBase from "@/components/MainBase/MainBase";
+
+export const metadata = {
+  title: "Viaja a New York | República Dominicana",
+  description: "En PedidosYa paga con Visa y gana",
+  icons: {
+    icon: "/favicon.png",
+  },
+  keywords:
+    "Pagos, PedidosYa Pagos, PedidosYa, Visa, Visa Crédito, Crédito, pedidosya, pedidos, rápido, compra, tarjeta, visa, crédito, débito, beneficios, compras, descuentos, gana, premio, concurso, oportunidades, supermercados, restaurantes, mercados, farmacias, tiendas, helados, mascotas, viajes, viajar, viaje, tour, new york, nueva york, manhattan, hamburguesa, burger",
+};
+
+const index = () => {
+  return (
+    <MainBase
+      dict={{
+        navbar: `ESTA PROMOCIÓN SÓLO ES VÁLIDA EN REPÚBLICA DOMINICANA
+        "Registrado por Pro-Consumidor bajo el No. CRS-0472/2023
+        Bases Legales Promoción
+        
+        1. DESCRIPCIÓN:  La promoción “Burger Tour” (Recorrido de la Hamburguesa), es organizado por Delivery Hero República Dominicana, S.R.L. (en lo adelante “PedidosYa” o el “Organizador”) y tiene por objeto premiar la fidelidad de nuestros clientes en todo el territorio de la República Dominicana (el Territorio) y será realizada entre el primero (1ro.) de agosto del 2023 hasta el cuatro (4) de septiembre del 2023 (Período de Vigencia).
+        2. REQUISITOS PARA PARTICIPAR: Podrán participar en la promoción los usuarios de PedidosYa, que sean personas físicas mayores de 18 años de edad, que tengan una cuenta en el Portal de PedidosYa (página web pedidosya.com.do o la Aplicación) de PedidosYa (el Portal), se encuentren logueados, con domicilio y residencia en el Territorio (los Participantes) y que cumplan con el mecanismo de participación indicado en estas Bases. 
+        2.1 Exclusiones: No podrán participar en la promoción las personas físicas menores de 18 años, las personas jurídicas, los empleados o directivos de PedidosYa, ni demás personas que PedidosYa contrate para proveer cualquier producto o cualquier servicio relacionado con la promoción, ni tampoco los cónyuges y los parientes por consanguinidad o afinidad hasta el segundo grado inclusive de cualquiera de los anteriores mencionados, sea que lo hagan en nombre propio o por cuenta y orden de terceros.
+        3. MECÁNICA:  Los Participantes deben realizar los siguientes pasos: (i) Ingresar con su usuario de PedidosYa al Portal. En caso de no contar con un usuario en “PedidosYa”, el Participante deberá registrarse y crear un usuario en el Portal. (ii) Realizar una compra efectiva con pedido confirmado en el Portal en los locales adheridos al concurso (en adelante los “Locales Participantes”), pagando con tarjeta de débito, crédito o prepago marca VISA (la Marca Auspiciadora). Los Locales Participantes son aquellos comercios adheridos al Portal de PedidosYa durante el Plazo de Vigencia en las secciones Restaurantes y Mercados; y, (iii) Con cada compra en los Locales Participantes, el Participante sumará oportunidades de ganar: 
+        Una (1) oportunidad en la sección “Restaurantes”, es decir, en los Locales Participantes que se encuentren en la categoría Restaurantes; 
+        Cinco (5) oportunidades en la sección “Mercados”, es decir, en los Locales Participantes que se encuentren en la categoría “Mercados”;
+        Diez (10) oportunidades si un usuario hace una compra en cualquiera de las secciones, por un monto igual o superior a ochocientos (RD$800.00) pesos dominicanos.
+        3.1 Para todos los efectos de estas Bases, queda entendido que “Compra Efectiva”, significa compra confirmada, no cancelada, sin devolución o solicitud de devolución, y el pedido entregado al usuario. Las compras que no cumplan con las condiciones para ser calificadas como efectivas no sumarán oportunidades para el usuario. 
+        3.4 Las oportunidades indicadas son acumulables sin importar el monto de la Compra Efectiva.
+        4. SELECCIÓN DE POTENCIAL GANADOR:
+        4.1. El Organizador conformará una base de datos con todas las oportunidades acumuladas por los participantes a lo largo del Plazo de Vigencia y realizará un sorteo para escoger al Potencial Ganador, el día seis (6) de septiembre de 2023 a las 4:00 pm., en el domicilio del Organizador, mediante un sistema aleatorio (en adelante, el “Sorteo”).
+        4.2. En el Sorteo será seleccionado un (01) ganador (en adelante, “Potencial Ganador”), y diez (10) potenciales ganadores alternos (en adelante los “Potenciales Ganadores Alternos”), para el caso de que quién resulte Potencial Ganador en primer término, no cumpliera con los requisitos de asignación establecidos en las presentes Bases. Los Potenciales Ganadores Alternos tendrán un orden de prelación que corresponderá al orden en el que salieron sorteados.
+        4.3 Una vez seleccionado el Potencial Ganador, será contactado por el Organizador dentro del plazo de siete (7) días contados a partir de haber sido realizado el Sorteo, a través del envío de un correo electrónico, a la dirección de correo electrónico que tiene registrado el Potencial Ganador en el Portal. El Potencial Ganador tendrá un plazo de veinticuatro (24) horas desde la recepción del mail para manifestar su aceptación al Premio, de no ser así el Premio se transferirá al Potencial Ganador Alterno.
+        5. PREMIO:
+        5.1. Quienes participen siguiendo la mecánica antes descrita, tendrán la posibilidad de ganar: (i) un boleto aéreo de ida y vuelta para 2 (dos) personas, Santo Domingo – New York, con las escalas, horarios y días que determine la aerolínea elegida por el Organizador a realizarse  del diecisiete (17) del mes de octubre del 2023 al veintiuno (21) del mes de octubre de 2023; (ii)  el hospedaje en un hotel a elección del Organizador, categoría 4 Estrellas por 4 (cuatro) noches, con desayuno incluido y con traslados hacia y desde el aeropuerto al hotel; y (iii) recorrido por 3 (tres) establecimientos de venta de hamburguesas de la ciudad de New York que serán seleccionados por el Organizador (en adelante, el “Premio”).
+        5.2. El Potencial Ganador tras recibir el correo electrónico del Organizador y dentro del plazo de veinticuatro (24) horas ya indicado, deberá enviar al Organizador la manifestación de su aceptación indicando en el correo electrónico: su nombre completo y número de teléfono celular de contacto. Anexando, además, a dicho correo electrónico de aceptación: 
+        (i) Copia de su Cédula de Identidad y Electoral o Carnet de Residencia Dominicana, en caso de ser extranjero, vigentes;
+         (ii) Copia de la Cédula de Identidad y Electoral o Carnet de Residencia Dominicana, en caso de ser extranjero, de su acompañante, quien debe ser mayor de 18 años de edad; 
+        (iii) Copia de su pasaporte y de su Visa o permiso de entrada a los Estados Unidos de América vigentes; 
+        (iv) Copia del pasaporte y de la Visa o permiso de entrada a los Estados Unidos de América de su acompañante vigentes. 
+        5.3 Tanto el Potencial Ganador como su acompañante, deberán poseer las vacunas necesarias para poder ingresar al lugar de destino y la documentación legal habilitante para salir del país y poder ingresar al país de destino.  Luego de que el Potencial Ganador manifieste su aceptación y envíe por correo electrónico los documentos antes indicados, será nuevamente contactado por el Organizador dentro del plazo de siete (7) días contados a partir de haber manifestado su voluntad con la documentación conforme, que le fue requerida, al número de teléfono celular proporcionado, para indicarle la fecha de entrega del Premio y los detalles del mismo, los cuales no podrán ser modificados, aunque sí podrán sufrir reprogramaciones por parte de las aerolíneas involucradas. 
+        6. CONDICIONES PARA LA ASIGNACIÓN DEL PREMIO:
+        6.1. El Organizador requerirá al Potencial Ganador, tanto de su persona como de su acompañante: a) Una copia escaneada de su Cédula de Identidad y Electoral o Carnet de Residencia Dominicana vigentes; b) Una copia escaneada de la Visa requerida o permiso de entrada que le permita ingresar al territorio de los Estados Unidos de América, que se encuentre vigente y con validez hasta el 31 de diciembre de 2023 al menos; c) Pasaporte vigente, y 6 meses de vigencia; d) otorgar una cesión de derechos de imagen y uso de datos personales, por parte de El Organizador, sin derecho a compensación alguna, durante el Plazo de Vigencia y hasta los tres (3) años posteriores a su finalización, a efectos de difundir el resultado de la Promoción y disfrute del Premio; e) será necesario que el Potencial Ganador y su acompañante contraten un seguro de asistencia al viajero; y f) cumplir con estas condiciones para la asignación del Premio que se establecen en estas Bases. En caso de cumplirse las presentes condiciones, El Potencial Ganador pasará a ser ganador del Premio (en adelante, “Ganador”). En caso contrario será descalificado y se contactará al Ganador Alterno.
+        6.2. El envío de documentación no conforme o el incumplimiento de cualquiera de las condiciones establecidas en estas Bases para la asignación del Premio hará perder automáticamente al Potencial Ganador el derecho a su asignación y será descalificado.
+        
+        7. ENTREGA DE PREMIOS:
+        El Premio será entregado al Ganador en el domicilio del Organizador. 
+        8. OTRAS CONDICIONES: 
+        8.1 El premio no es transferible, ni podrá requerir un cambio de las especificaciones de este, o su sustitución por el equivalente en dinero.
+        8.2 Luego de informados los datos del Ganador y su acompañante, no será posible realizar cambios.
+        8.3 Los Participantes son responsables de la veracidad de toda la información suministrada. En caso de que se detectase alguna falsedad en la información suministrada, PedidosYa descalificará al Participante de que se tratase.
+        8.4 Mediante la participación en la Concurso y la aceptación de estos términos y condiciones, el Participante libra a PedidosYa y sus respectivas afiliadas, accionistas, directores, funcionarios y empleados, así como a los Locales Participantes y la Marca Auspiciadora, de responsabilidad de cualquier perjuicio, pérdida o daños causados por caso fortuito, fuerza mayor, falta imputable a un tercero o imprudencia del participante o que se deriven de uso o mal uso de los premios una vez estos sean entregados. 
+        8.5 PedidosYa no será responsable por los gastos en que incurrieren los Participantes en cualquier etapa de la Promoción ni disfrute del Premio que no se encuentre expresamente contemplado en la cláusula 5.1 de las presentes Bases. Asimismo, estará a cargo del todo impuesto, tasa o contribución, nacional, provincial o municipal, que deba tributarse con relación a los premios o con motivo de la organización de la presente Promoción.
+        8.6 Al participar en esta Promoción y aceptar el premio, cada ganador y su acompañante, manifiesta su conformidad con que PedidosYa, sus subsidiarias, filiales, matriz, agencias de publicidad, de promociones o de relaciones públicas, utilicen sus datos personales, voz e imagen por un período de tres (3) años contados a partir de la fecha de su elección como ganador para publicar la adjudicación del premio y publicitar la Promoción, sin límite territorial, en prensa escrita, televisión, radio, redes sociales y páginas web, sin ninguna compensación, ni necesidad de notificación o permiso adicional a esta aceptación. Asimismo, los participantes aceptan que los datos personales, incluyendo su nombre y dirección, se procesarán y almacenarán para los propósitos de la Promoción. PedidosYa también podrá utilizar los datos para verificar la identidad, y datos de los Participantes o para comprobar de alguna otra forma que éstos reúnen las condiciones para participar en la Promoción. 
+        8.7 La participación en este Concurso implica el conocimiento y aceptación de estas Bases legales, para cualquier cuestión no prevista en las mismas se remitirán al derecho común y las instancias administrativas creadas al efecto.
+        8.8 PedidosYa podrá modificar, cancelar o suspender en forma inmediata la presente Concurso, sin asumir responsabilidad alguna, por cualquier causa, incluyendo si se llegasen a detectar fraudes o si se presentara una circunstancia de fuerza mayor, previa comunicación y aprobación de la Dirección Ejecutiva de Pro-Consumidor.
+        8.9 PedidosYa, tiene sus oficinas corporativas en la Avenida Gustavo Mejía Ricart No 81, Edificio Corporativo OV, piso 6, sector Piantini, en la ciudad de Santo Domingo, D.N. República Dominicana. En caso de requerir más información, realizar contacto a través de la sección “Atención al Cliente” del portal y adicionalmente en la instancia de "Preguntas frecuentes". Estas Bases estarán disponibles y podrán ser consultadas durante el Periodo de Vigencia en www.pedidosya.com.do
+        `,
+        hero: {
+          title:
+            "En PedidosYa paga con Visa y gana un viaje a New York, para probar hamburguesas en Manhattan.",
+          imageRegion: "Pe",
+        },
+        travel: {
+          title: "Gánate un viaje a New York",
+          subtitle: "y prueba hamburguesas en Manhattan",
+          bullets: [
+            "Viaje para 2 personas",
+            "Vuelos ida y regreso a New York",
+            "4 noches de estadía en hotel 4 estrellas",
+          ],
+        },
+        howToWin: {
+          title: "En PedidosYa paga con Visa",
+          subtitle: "Y empieza a viajar antes de viajar",
+          dates: {
+            firstLine:
+              "Acumula oportunidades desde el 1/8/2023 hasta el 4/9/2023",
+            secondLine: "El sorteo se realizará el 6/9/2023",
+          },
+          buttonTitle: "Empieza ya a sumar oportunidades",
+          opportunities: {
+            span: "¡Acelera tus oportunidades!",
+            plusTen: {
+              numTitle: "oportunidades",
+              text: "Tus pedidos mayores a $800 suman más",
+            },
+            plusFive: {
+              numTitle: "oportunidades",
+              text: "Por tus pedidos en Mercados y tiendas",
+            },
+            plusOne: {
+              numTitle: "oportunidad",
+              text: "Por tus pedidos en Restaurantes",
+            },
+          },
+          link: "https://pwdh.adj.st/?adjust_t=84samxj_ec0dled&adjust_campaign=other__other_mix_eat_ma_fintech_visa&adjust_deeplink=pedidosya%3A%2F%2Fwww.pedidosya.com.pe/&adjust_fallback=https%3A%2F%2Fwww.pedidosya.com.pe%2F%3F%26pycat%3Dother%26utm_source%3Dweb-referral%26utm_medium%3Dreferral%26utm_campaign%3Dother_other_mix_eat_ma_fintech_visa",
+          appDownload: "Descarga la app",
+        },
+        footer: {
+          footerTitle: "Descarga la app",
+          footerDownloadAndroid:
+            "https://play.google.com/store/apps/details?id=com.pedidosya&hl=es_AR&gl=US",
+          footerDownloadApple:
+            "https://apps.apple.com/cl/app/pedidosya-delivery-app/id490099807",
+        },
+      }}
+    />
+  );
+};
+
+export default index;
